7e6c45893ccb07fd8b7025d8a9a6360f4e84153d
阿彬 <2242936124@qq.com>
2024-07-06 09:33:07 +0800

    去除ResNet50x4

--- models/image_encoders/__init__.py
+++ models/image_encoders/__init__.py
@@ -2,7 +2,7 @@ from typing import Tuple
 
 from trainers.abc import AbstractBaseImageLowerEncoder, AbstractBaseImageUpperEncoder
 from models.image_encoders.resnet import ResNet18Layer4Lower, ResNet18Layer4Upper, ResNet50Layer4Lower, \
-    ResNet50Layer4Upper,ResNet50x4Lower, ResNet50x4Upper
+    ResNet50Layer4Upper
 from models.image_encoders.modified_resnet import ModifiedResNetLower, ModifiedResNetUpper
 from models.image_encoders.vit import VisionTransformerLower, VisionTransformerUpper
 
@@ -23,11 +23,6 @@ def image_encoder_factory(config: dict) -> Tuple[AbstractBaseImageLowerEncoder, 
         lower_feature_shape = lower_encoder.layer_shapes()['layer4']
         upper_encoder = ResNet50Layer4Upper(lower_feature_shape, feature_size, pretrained=pretrained,
                                             norm_scale=norm_scale)
-    elif model_code == 'resnet50x4_layer4':
-        lower_encoder = ResNet50x4Lower(pretrained, stride=config.get('stride', False))
-        lower_feature_shape = lower_encoder.layer_shapes()['layer4']
-        upper_encoder = ResNet50x4Upper(lower_feature_shape, feature_size, pretrained=pretrained,
-                                        norm_scale=norm_scale)
     elif model_code == 'modified_resnet':
         lower_encoder = ModifiedResNetLower(pretrained)
         lower_feature_shape = lower_encoder.layer_shapes()['layer4']
